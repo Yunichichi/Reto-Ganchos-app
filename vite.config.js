8df65f6a9ee5c35e081c707c343ef4e9031dca85
Yunior <yuniorproduc@gmail.com>
2025-08-31 20:42:29 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Aplicación Reto-Ganchos",
   plugins: [react()],
   base: '/ganchos-app/',
 })
